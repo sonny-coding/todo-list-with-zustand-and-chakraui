604f807d6830737618281402aa3cae18a79e5adc
Son Dao <udemydaohsn@gmail.com>
2022-07-25 16:10:41 -0700

    add form to Chakraui

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,7 @@ import {
   Input,
   InputRightElement,
   IconButton,
+  Box,
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import { useStore } from "../stores";
@@ -12,27 +13,37 @@ function TodoForm() {
   const [text, setText] = useState("");
   const addTodo = useStore((state) => state.addTodo);
   return (
-    <InputGroup onSubmit={() => addTodo(text)}>
-      <Input
-        value={text}
-        placeholder="add todo..."
-        size="lg"
-        variant={"flushed"}
-        onChange={(evt) => setText(evt.target.value)}
-      />
-      <InputRightElement
-        onClick={() => addTodo(text)}
-        children={
-          <IconButton
-            // mr="2"
-            mt="2"
-            colorScheme="blue"
-            aria-label="Search database"
-            icon={<AddIcon />}
+    <Box w="100%">
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          setText("");
+          addTodo(text);
+        }}
+      >
+        <InputGroup>
+          <Input
+            value={text}
+            placeholder="add todo..."
+            size="lg"
+            variant={"flushed"}
+            onChange={(evt) => setText(evt.target.value)}
           />
-        }
-      />
-    </InputGroup>
+          <InputRightElement
+            onClick={() => addTodo(text)}
+            children={
+              <IconButton
+                // mr="2"
+                mt="2"
+                colorScheme="blue"
+                aria-label="Search database"
+                icon={<AddIcon />}
+              />
+            }
+          />
+        </InputGroup>
+      </form>
+    </Box>
   );
 }
 
